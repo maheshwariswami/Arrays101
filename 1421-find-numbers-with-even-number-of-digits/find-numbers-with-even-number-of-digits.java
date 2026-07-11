e176542ae8721ede6fbb4f73817b2f770814b5ab
maheshwariswami <55083499+maheshwariswami@users.noreply.github.com>
2026-07-11 21:54:44 +0530

    Time: 1 ms (99.54%) | Memory: 44.5 MB (78.73%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -3,6 +3,7 @@ class Solution {
         int countEventNo=0;
        // nums = [12,345,2,6,7896]
        //
+       /*
         for(int i=0;i< nums.length ;i++)
         {
             int digit = String.valueOf(nums[i]).length();
@@ -10,5 +11,19 @@ class Solution {
                 countEventNo++;
         }
         return countEventNo;
+        */
+
+        //2nd approach
+         for(int i=0;i< nums.length ;i++)
+        {
+            if((nums[i]>=10 && nums[i]<=99) ||
+            (nums[i]>=1000 && nums[i]<=9999) ||
+            (nums[i]==999999 || nums[i]==100000 ) 
+             )
+             {
+                countEventNo++;
+             }
+        }
+           return countEventNo;
     }
 }
